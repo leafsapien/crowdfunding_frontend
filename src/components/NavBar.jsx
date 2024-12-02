b90ae525aa08023196c11a7e085b69da8729dd8c
Anaya <anaya.basak@gmail.com>
2024-12-02 12:03:36 +1000

    Added to navbar: Logged in/out ternary, if logged in: Home, My Details, Log out.  If not a logged in user: Home, Sign Up, Log in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,12 +13,18 @@ function NavBar() {
         <div>
         <nav>
             <Link to="/">Home</Link>
-            {auth.token ? (
+            {!auth.token ? ( //If user is not logged in
+            <>
+                <Link to="/signup">Sign Up</Link>
+                <Link to="/login">Log In</Link>
+            </>
+            ) : ( // If User is logged in
+            <>
+                <Link to="/mydetails">My Details</Link>
                 <Link to="/" onClick={handleLogout}>
                     Log Out
                 </Link>
-            ) : (
-                <Link to="/login">Log In</Link>
+            </>
             )}
         </nav>
         <Outlet />
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,5 @@
+function SignupPage() {
+    return <h1>This is the signup page</h1>;
+    }
+    
+export default SignupPage;
