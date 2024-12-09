56cdb55203a287257eb9d26e433ede714b4af1fc
Anaya <anaya.basak@gmail.com>
2024-12-09 12:18:37 +1000

    Added project id capture to use-pledge hook to send to back end.

--- src/hooks/use-pledge.js
+++ src/hooks/use-pledge.js
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react';
 import getPledge from '../api/get-pledge';
 
-export default function usePledge(pledgeID) {
-    const [pledge, setpledge] = useState();
+export default function usePledge(pledgeID, projectID) {
+    const [pledge, setPledge] = useState();
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState();
 
     useEffect(() => {
+        if (!pledgeID || projectID) return; //Prevents unnecessary calls
+
         // Here we pass the pledgeID to the getpledge function.
-        getpledge(pledgeID)
+        getPledge(pledgeID, projectID)
             .then((pledge) => {
-                setpledge(pledge);
+                setPledge(pledge);
                 setIsLoading(false);
             })
             .catch((error) => {
@@ -19,7 +21,7 @@ export default function usePledge(pledgeID) {
             });
 
         // This time we pass the pledgeID to the dependency array so that the hook will re-run if the pledgeID changes.
-    }, [pledgeID]);
+    }, [pledgeID, projectID]);
 
     return { pledge, isLoading, error };
 }
