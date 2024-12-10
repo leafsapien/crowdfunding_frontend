20448040d1a57baa8e65aed056edddd65708bd36
Anaya <anaya.basak@gmail.com>
2024-12-10 13:34:22 +1000

    Added get users api to obtain username against user id

---
+++ src/api/get-users.js
@@ -0,0 +1,18 @@
+async function getUsers(userID) {
+    const url = `${import.meta.env.VITE_API_URL}/users/${userID}`;
+    const response = await fetch(url, { method: 'GET' });
+
+    if (!response.ok) {
+        const fallbackError = `Error fetching User with id ${userID}`;
+
+        const data = await response.json().catch(() => {
+            throw new Error(fallbackError);
+        });
+
+        throw new Error(data?.detail || fallbackError);
+    }
+
+    return await response.json();
+}
+
+export default getUsers;
