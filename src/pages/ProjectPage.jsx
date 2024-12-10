6841d37f094059d9ff80a100f7caf2ac8f4a979f
Anaya <anaya.basak@gmail.com>
2024-12-10 13:33:46 +1000

    Added project funding goal progress by adding current pledge amounts together

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -2,14 +2,12 @@ import { useParams } from 'react-router-dom';
 import useProject from '../hooks/use-project';
 import PledgeForm from '../components/PledgeForm';
 import PledgeCard from '../components/PledgeCard';
-import usePledges from '../hooks/use-pledges';
+import usePledge from '../hooks/use-pledge';
 
 function ProjectPage() {
     const { id } = useParams();
     const { project, isLoading, error } = useProject(id);
-    // const { pledges, pledgeIsLoading, pledgeError } = usePledges();
-
-    
+    const { pledge } = usePledge(id);
 
     if (isLoading ) {
         return <p>Loading...</p>;
@@ -17,17 +15,26 @@ function ProjectPage() {
     if (error) {
         return <p>Error: {error.message}</p>;
     }
-    // if (pledgeError) {
-    //     return <p>Pledges error: {pledgeError.message} </p>
-    // }
 
     const pledges = project.pledges;
+
+    function totalGoal (pledges) {
+
+        const totalGoal = pledges.reduce((sum, pledge) => {
+            return sum + (pledge.amount || 0);
+        }, 0);
+        return totalGoal;
+    }
 
     return (
         <div>
             <h2>{project.title}</h2>
             <h3>Created at: {project.date_created}</h3>
             <h3>{`Status: ${project.is_open ? 'Open' : 'Closed'}`}</h3>
+            <img src={project.image} />
+            <p>{project.description}</p>
+            <h4>Project Funding Goal: ${project.goal}</h4>
+            <h4>Current Goal Status: ${totalGoal(pledges)}</h4>
 
             <h3>Pledges:</h3>
                 <ul>
